34304dcaa53bc3931ccaf43612b5ea2ebfa4822b
Quentin Plet <quentin.plet@gmail.com>
2024-02-19 23:41:03 +0100

    ♻️ Button > Create Button Component

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,3 +1,15 @@
-export default function Button() {
-  return <></>;
+interface ButtonProps {
+  children: React.ReactNode;
+  handleClick: () => void;
+}
+
+export default function Button({ children, handleClick }: ButtonProps) {
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-cyanLight max-w-28 px-4 py-1 rounded-lg border-2 border-cyanDark  hover:bg-cyanDark hover:border-cyanLight"
+    >
+      <span className="font-primary font-bold text-sm">{children}</span>
+    </button>
+  );
 }
---
+++ src/router/scrollToTop.ts
@@ -0,0 +1,16 @@
+import { FC, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop: FC = () => {
+  // Extracts pathname property(key) from an object
+  const { pathname } = useLocation();
+
+  // Automatically scrolls to top whenever pathname changes
+  useEffect(() => {
+    // window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return null;
+};
+
+export default ScrollToTop;
